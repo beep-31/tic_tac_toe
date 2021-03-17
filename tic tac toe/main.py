93e8306fcdb0ef5d73cab6fd99f5922630cd824c
beep-31 <alex.scheglov25@gmail.com>
2021-03-17 13:59:29 +0100

    Diagonal check was added

--- tic tac toe/main.py
+++ tic tac toe/main.py
@@ -1,5 +1,5 @@
-game = [[1, 1, 1],
-        [1, 0, 0],
+game = [[0, 1, 1],
+        [1, 1, 0],
         [0, 1, 1]]
 
 
@@ -8,7 +8,7 @@ def game_board(game_map, player=0, row=0, column=0):
     try:
         print("   0  1  2")
         game_map[row][column] = player
-        for count, row in enumerate(game_map):
+        for count, row in enumerate(reversed(game_map)):
             print(count, row)
     except IndexError as e:
         print("Something went wrong! Make sure you input row/column as 0 1 or 2?", e)
@@ -16,6 +16,7 @@ def game_board(game_map, player=0, row=0, column=0):
         print("The program is done running")
 
 
+"""
 def win(current_game):
     win = False
     for col in range(len(current_game)):
@@ -25,7 +26,22 @@ def win(current_game):
         if check.count(check[0]) == len(check) and check[0] != 0:
             print("Winner")
             break
+"""
 
 
-# game_board(game_map=game, player=1, row=1, column=1)
-win(current_game=game)
+def diagonal_win(current_game):
+    diags_rev = []
+    diags = []
+    rows = range(len(current_game))
+    cols = reversed(range(len(current_game)))
+    for row, col in zip(rows, cols):
+        print(row, col)
+        diags_rev.append(current_game[row][col])
+        diags.append(current_game[row][row])
+    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+        print('Winner')
+    elif diags_rev.count(diags_rev[0]) == len(diags_rev) and diags_rev[0] != 0:
+        print('Winner')
+
+
+diagonal_win(game)
