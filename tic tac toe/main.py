55c70e4b0fd143ec7adbb4393ca87631133b06e6
beep-31 <alex.scheglov25@gmail.com>
2021-03-23 11:59:27 +0100

    The game was completely finished

--- tic tac toe/main.py
+++ tic tac toe/main.py
@@ -1,47 +1,110 @@
-game = [[0, 1, 1],
-        [1, 1, 0],
-        [0, 1, 1]]
+import itertools
 
 
-def game_board(game_map, player=0, row=0, column=0):
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
 
-    try:
-        print("   0  1  2")
-        game_map[row][column] = player
-        for count, row in enumerate(reversed(game_map)):
+    if just_display == True:
+        print("   "+"  ".join([str(i) for i in range(len(game_map))]))
+        for count, row in enumerate((game_map)):
             print(count, row)
-    except IndexError as e:
-        print("Something went wrong! Make sure you input row/column as 0 1 or 2?", e)
-    finally:
-        print("The program is done running")
+    else:
+        try:
+            if game_map[row][column] != 0:
+                print('This position is ocupated!')
+                return game_map, False
+            game_map[row][column] = player
+            print("   0  1  2")
+            for count, row in enumerate((game_map)):
+                print(count, row)
+            return game_map, True
+        except IndexError as e:
+            print("Something went wrong! Make sure you input row/column as 0 1 or 2?", e)
+            return game_map, False
 
 
-"""
-def win(current_game):
-    win = False
+def all_same(l):
+    if l.count(l[0]) == len(l) and l[0] != 0:
+        return True
+    else:
+        return False
+
+
+# Function which detects a horizontal winner
+def horizontal_win(current_game):
+    for row in current_game:
+        if all_same(row):
+            print("Winner horizontal")
+            return True
+
+# Function which detects vertical winner
+
+
+def vertical_win(current_game):
     for col in range(len(current_game)):
         check = []
         for row in current_game:
             check.append(row[col])
-        if check.count(check[0]) == len(check) and check[0] != 0:
-            print("Winner")
-            break
-"""
+        if all_same(check):
+            print("Winner vertical")
+            return True
 
 
+# Function which detects diagonal winner
 def diagonal_win(current_game):
     diags_rev = []
     diags = []
     rows = range(len(current_game))
     cols = reversed(range(len(current_game)))
     for row, col in zip(rows, cols):
-        print(row, col)
         diags_rev.append(current_game[row][col])
         diags.append(current_game[row][row])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
-        print('Winner')
-    elif diags_rev.count(diags_rev[0]) == len(diags_rev) and diags_rev[0] != 0:
-        print('Winner')
+    if all_same(diags):
+        print('Winner diagonal (/)')
+        return True
+    elif all_same(diags_rev):
+        print('Winner diagonal (\\)')
+        return True
 
 
-diagonal_win(game)
+def win(current_game):
+    if diagonal_win(current_game):
+        return True
+    elif vertical_win(current_game):
+        return True
+    elif horizontal_win(current_game):
+        return True
+    else:
+        return False
+
+
+play = True
+players = [1, 2]
+while play:
+    game_size = 3
+    game = [[0 for i in range(game_size)] for i in range(game_size)]
+    game_won = False
+    game_board(game, just_display=True)
+    player_choice = itertools.cycle(players)
+    while not game_won:
+        current_player = next(player_choice)
+        print(f"Current Player: {current_player}")
+        played = False
+
+        while not played:
+            column_choice = int(
+                input("What column do you want to play? (0,1,2) : "))
+            row_choice = int(input("What row do you want to play? (0,1,2) : "))
+            game, played = game_board(
+                game, current_player, row_choice, column_choice)
+            if win(game):
+                game_won = True
+                again = input(
+                    "The game is over, would you like to play again? (y/n)")
+                if again.lower() == 'y':
+                    print("Restarting")
+                elif again.lower() == 'n':
+                    print("Bye bye!")
+                    play = False
+                else:
+                    print("Invalid answer, c u l8r mmgvo")
+                    play = False
